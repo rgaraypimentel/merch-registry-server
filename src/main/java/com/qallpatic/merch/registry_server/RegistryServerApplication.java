571fcedaf22984e8d9bb25b4a51f888e74f37cdb
cathyhh <a20207995@pucp.edu.pe>
2025-02-28 08:34:19 -0500

    Primer commit

---
+++ src/main/java/com/qallpatic/merch/registry_server/RegistryServerApplication.java
@@ -0,0 +1,13 @@
+package com.qallpatic.merch.registry_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qallpatic/merch/registry_server/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qallpatic.merch.registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
